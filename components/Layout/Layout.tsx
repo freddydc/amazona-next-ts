82a4cf35b78254c44405a222acae0e2492b6643d
freddydc <freddydiaz.liv@gmail.com>
2021-07-30 12:10:03 -0500

    Add Styles To Layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import Head from 'next/head';
 import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
+import useStyles from '../../utils/styles';
 
 type LayoutProps = {
   children?: any;
 };
 
 const Layout = ({ children }: LayoutProps) => {
+  const classes = useStyles();
+
   return (
     <div>
       <Head>
         <title>Magazine</title>
       </Head>
-      <AppBar position="static">
+      <AppBar className={classes.navbar} position="static">
         <Toolbar>
           <Typography>Magazine</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
         <Typography>All rights reserved. Magazine</Typography>
       </footer>
     </div>
