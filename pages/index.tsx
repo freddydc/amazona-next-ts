2d62eb8796c7fcc74a2696bfc3fb8d0050a911ea
freddydc <freddydiaz.liv@gmail.com>
2021-08-01 18:36:38 -0500

    List Products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,45 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@material-ui/core';
 import Layout from '../components/Layout/Layout';
+import data from '../utils/data/data';
 
 const Home = () => {
   return (
     <Layout>
       <div>
         <h1>Products</h1>
-        <ul>
-          <li>Shoes</li>
-          <li>Jackets</li>
-          <li>Shirts</li>
-        </ul>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  ></CardMedia>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>${product.price}</Typography>
+                  <Button size="small" color="primary">
+                    Add To Cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
   );
